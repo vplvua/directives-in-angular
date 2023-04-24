281f59ba3bcd8de022d645c4e1293e8d7804ff1c
vasyl.pashko <vasyl.pashko@gmail.com>
2023-04-24 07:14:02 +0300

    new directive

---
+++ src/app/times.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appTimes]',
+})
+export class TimesDirective {
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef
+  ) {}
+
+  @Input() set appTimes(count: number) {
+    this.viewContainer.clear();
+
+    for (let i = 0; i < count; i++) {
+      this.viewContainer.createEmbeddedView(this.templateRef, {});
+    }
+  }
+}
